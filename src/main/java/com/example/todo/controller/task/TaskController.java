1a75e50713ef35027b3a324f7d5bffbbc36007d4
east-forest <bigtree1013@icloud.com>
2023-10-02 21:18:41 +0900

    修正内容 Entityクラス、DTOクラスの分別 ServiceクラスをBeanに登録と取得 Lombokを利用できるようにする

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,19 +1,24 @@
 package com.example.todo.controller.task;
 
+import com.example.todo.service.task.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final TaskService taskService;
 
     @GetMapping("/tasks")
     public String list(Model model) {
-        var task1 = new TaskDTO(1L, "Spring Bootを学ぶ", "TODOアプケーションを作る", "TODO");
-        var task2 = new TaskDTO(2L, "Spring Securityを学ぶ", "ログイン機能を作る", "TODO");
-        var taskList = List.of(task1, task2);
+        // List<TaskEntity> -> List<TaskDTO>
+        var taskList = taskService.findAll()
+                .stream()
+                .map(TaskDTO::toDto).toList();
+
         model.addAttribute("taskList", taskList);
         return "tasks/list";
     }
